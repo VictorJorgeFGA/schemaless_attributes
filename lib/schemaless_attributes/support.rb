6eb6078ff39dc8b5d9737ea64c380c174edb0d46
VictorJorgeFGA <victor.eng.unb@gmail.com>
2024-01-07 22:11:58 -0300

    Fixes module extend

--- lib/schemaless_attributes/support.rb
+++ lib/schemaless_attributes/support.rb
@@ -8,17 +8,18 @@ module SchemalessAttributes
   # This concern add the ability to define json_attributes and attachment_attributes inside of your model.
   # It saves the registered attributes and handle all the logic behind storing and converting data.
   #
-  class Support
-    include ActiveSupport::Concern
+  module Support
+    extend ActiveSupport::Concern
 
     @registered_schemaless_attributes = []
 
-    class << self
+    class_methods do
       def registered_schemaless_attributes
         @registered_schemaless_attributes.dup
       end
 
-      def json_attribute(name, type:, source: default_json_attributes_source)
+      def json_attribute(name, type:, source: nil)
+        source = default_json_attributes_source if source.nil?
         type = type.to_sym
         name = name.to_s
 
@@ -55,20 +56,20 @@ module SchemalessAttributes
         end
       end
 
-      private_class_method def check_and_register_attribute!(name, type, source, expected_source_type:)
+      def check_and_register_attribute!(name, type, source, expected_source_type:)
         check_if_attribute_name_is_ambiguos!(name)
         check_if_source_exists_and_has_correct_type!(name, source, expected_type: expected_source_type)
         check_if_attribute_type_is_valid!(name, type)
         register_attribute(name)
       end
 
-      private_class_method def check_if_attribute_name_is_ambiguos!(attribute_name)
+      def check_if_attribute_name_is_ambiguos!(attribute_name)
         if instance_methods.include?(attribute_name.to_sym) || instance_methods.include?("#{attribute_name}=".to_sym)
           raise "Declaration of schemaless attribute with ambiguous name \"#{attribute_name}\" on #{self.class}!"
         end
       end
 
-      private_class_method def check_if_source_exists_and_has_correct_type!(attribute_name, source, expected_type:)
+      def check_if_source_exists_and_has_correct_type!(attribute_name, source, expected_type:)
         unless instance_methods.include?(source)
           raise "The provided source \"#{source}\" for schemaless attribute \"#{attribute_name}\" is not defined!"
         end
@@ -79,15 +80,19 @@ module SchemalessAttributes
               "#{send(source).class}, it was expected to be a #{expected_type}!"
       end
 
-      private_class_method def check_if_attribute_type_is_valid!(attribute_name, type)
+      def check_if_attribute_type_is_valid!(attribute_name, type)
         return if SchemalessAttributes::Type.type_available?(type)
 
         raise "Schemaless attribute \"#{attribute_name}\" has unsupported type: #{type}!"
       end
 
-      private_class_method def register_attribute(attribute_name)
+      def register_attribute(attribute_name)
         @registered_schemaless_attributes << attribute_name.to_s
       end
+    end
+
+    def default_json_attributes_source
+      raise 'Need to be implemented!'
     end
   end
 end
